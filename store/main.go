570528007ddd9c08b9c3670141c7864947835465
Cyrill Schumacher <cyrill@schumacher.fm>
2015-08-15 21:47:29 +0200

    CSMS: restructuring

--- store/main.go
+++ store/main.go
@@ -25,11 +25,11 @@ import (
 	"github.com/corestoreio/csfw/config"
 	"github.com/corestoreio/csfw/net"
 	"github.com/corestoreio/csfw/storage/csdb"
+	"github.com/corestoreio/csfw/storage/dbr"
 	"github.com/corestoreio/csfw/store"
 	"github.com/corestoreio/csfw/user/userjwt"
 	"github.com/corestoreio/csfw/utils/log"
 	"github.com/labstack/echo"
-	"github.com/pborman/uuid"
 )
 
 const ServerAddress = "127.0.0.1:3010"
@@ -49,37 +49,29 @@ func main() {
 		log.Fatal("config.DefaultManager.ApplyCoreConfigData", "err", err)
 	}
 
-	sm := store.NewManager(
-		store.NewStorageOption(),
-	)
-	if err := sm.ReInit(dbc.NewSession()); err != nil {
-		log.Fatal("sm.ReInit(dbc.NewSession())", "err", err)
-	}
-
 	e := echo.New()
 	e.SetHTTPErrorHandler(net.RESTErrorHandler)
-
-	jwtMng, err := userjwt.New()
-	if err != nil {
-		log.Fatal("userjwt.New", "err", err)
-	}
-
-	eg1 := e.Group(net.APIRoute.String(), jwtMng.Authenticate)
 
 	//	e.Use(mw.Logger())
 	//e.Use(mw.Recover())
 	//	e.SetDebug(true)
 
-	eg1.Get(store.RouteStores, store.RESTStores(sm))
-	//	eg1.POST(store.RouteStores, store.RESTStoreCreate)
-	//	eg1.GET(store.RouteStore, store.RESTStore)
-	//	eg1.PUT(store.RouteStore, store.RESTStoreSave)
-	//	eg1.DELETE(store.RouteStore, store.RESTStoreDelete)
-
-	e.Get("/login", routeLogin(jwtMng))
+	eg1 := setupAuth(e)
+	setupStoreRoutes(eg1, dbc.NewSession())
 
 	println("Starting server @ ", ServerAddress)
 	e.Run(ServerAddress)
+}
+
+func setupAuth(e *echo.Echo) *echo.Group {
+	jwtMng, err := userjwt.New()
+	if err != nil {
+		log.Fatal("userjwt.New", "err", err)
+	}
+	jwtMng.EnableJTI = true
+	e.Get("/login", routeLogin(jwtMng))
+	return e.Group(net.APIRoute.String(), jwtMng.Authenticate)
+
 }
 
 // just hacked into it. @todo: auth from admin_user table and role checking
@@ -87,7 +79,6 @@ func routeLogin(jm *userjwt.AuthManager) echo.HandlerFunc {
 
 	staticClaims := map[string]interface{}{
 		"xfoo":  "bar",
-		"xbuzz": uuid.New(),
 		"xtime": time.Now().Unix(),
 	}
 
@@ -99,4 +90,20 @@ func routeLogin(jm *userjwt.AuthManager) echo.HandlerFunc {
 
 		return c.String(http.StatusOK, ts)
 	}
+}
+
+func setupStoreRoutes(g *echo.Group, dbrSess *dbr.Session) {
+	sm := store.NewManager(
+		store.NewStorageOption(),
+	)
+	if err := sm.ReInit(dbrSess); err != nil {
+		log.Fatal("sm.ReInit(dbc.NewSession())", "err", err)
+	}
+
+	g.Get(store.RouteStores, store.RESTStores(sm))
+	//	eg1.POST(store.RouteStores, store.RESTStoreCreate)
+	//	eg1.GET(store.RouteStore, store.RESTStore)
+	//	eg1.PUT(store.RouteStore, store.RESTStoreSave)
+	//	eg1.DELETE(store.RouteStore, store.RESTStoreDelete)
+
 }
