e87bb742f56aaa025c162b6ae39190c56ff95c66
Cyrill Schumacher <cyrill@schumacher.fm>
2015-11-21 17:19:04 +0100

    Refactoring to match new API. Echo pkg will be removed.

--- store/main.go
+++ store/main.go
@@ -14,98 +14,127 @@
 
 package main
 
-/*
-	Experimental micro service app to handle package store
-
-	Use github.com/vdobler/ht for testing
-*/
+//	Experimental micro service app to handle package store
 
 import (
 	"net/http"
 	"time"
 
 	"github.com/corestoreio/csfw/config"
-	"github.com/corestoreio/csfw/net"
+	"github.com/corestoreio/csfw/config/scope"
+	"github.com/corestoreio/csfw/net/ctxjwt"
+	"github.com/corestoreio/csfw/net/httputils"
 	"github.com/corestoreio/csfw/storage/csdb"
 	"github.com/corestoreio/csfw/storage/dbr"
 	"github.com/corestoreio/csfw/store"
-	"github.com/corestoreio/csfw/user/userjwt"
 	"github.com/corestoreio/csfw/utils/log"
 	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
 )
 
 const ServerAddress = "127.0.0.1:3010"
 
 func init() {
-	log.Set(log.NewStdLogger())
+	log.PkgLog = log.NewStdLogger(
+		log.SetStdLevel(log.StdLevelInfo),
+	)
+	store.PkgLog = log.PkgLog
+	config.PkgLog = log.PkgLog
+	csdb.PkgLog = log.PkgLog
+	dbr.PkgLog = log.PkgLog
 }
 
-func main() {
-	dbc, err := csdb.Connect()
-	if err != nil {
+type app struct {
+	dbc      *dbr.Connection
+	jwtSrv   *ctxjwt.Service
+	storeSrv *store.Service
+}
+
+// newApp creates a new application. function can only be called once
+func newApp() *app {
+	a := new(app)
+	var err error
+
+	// make sure env var CS_DSN is set and points to the appropriate database
+	if a.dbc, err = csdb.Connect(); err != nil {
 		log.Fatal("MySQL Connect", "err", err)
 	}
-	defer dbc.Close() // @todo check signal and close gracefully
 
-	if err := config.DefaultManager.ApplyCoreConfigData(dbc.NewSession()); err != nil {
+	if err := config.TableCollection.Init(a.dbc.NewSession()); err != nil {
+		log.Fatal("config.TableCollection.Init", "err", err)
+	}
+	if err := store.TableCollection.Init(a.dbc.NewSession()); err != nil {
+		log.Fatal("store.TableCollection.Init", "err", err)
+	}
+
+	// load config data from core_config_data SQL table
+	if err := config.DefaultManager.ApplyCoreConfigData(a.dbc.NewSession()); err != nil {
 		log.Fatal("config.DefaultManager.ApplyCoreConfigData", "err", err)
 	}
 
-	e := echo.New()
-	e.SetHTTPErrorHandler(net.RESTErrorHandler)
-
-	//	e.Use(mw.Logger())
-	//e.Use(mw.Recover())
-	//	e.SetDebug(true)
-
-	eg1 := setupAuth(e)
-	setupStoreRoutes(eg1, dbc.NewSession())
-
-	println("Starting server @ ", ServerAddress)
-	e.Run(ServerAddress)
-}
-
-func setupAuth(e *echo.Echo) *echo.Group {
-	jwtMng, err := userjwt.New()
-	if err != nil {
-		log.Fatal("userjwt.New", "err", err)
+	// create JSON web token instance
+	if a.jwtSrv, err = ctxjwt.NewService(); err != nil {
+		log.Fatal("ctxjwt.NewService", "err", err)
 	}
-	jwtMng.EnableJTI = true
-	e.Get("/login", routeLogin(jwtMng))
-	return e.Group(net.APIRoute.String(), jwtMng.Authorization)
+	a.jwtSrv.EnableJTI = true
 
+	if a.storeSrv, err = store.NewService(
+		scope.Option{Website: scope.MockID(1)}, // run website ID 1, see database table
+		store.MustNewStorage(store.WithDatabaseInit(a.dbc.NewSession())),
+	); err != nil {
+		log.Fatal("store.NewService", "err", err)
+	}
+
+	return a
 }
 
-// just hacked into it. @todo: auth from admin_user table and role checking
-func routeLogin(jm *userjwt.AuthManager) echo.HandlerFunc {
+func (a *app) close() {
+	if err := a.dbc.Close(); err != nil {
+		log.Fatal("MySQL Close", "err", err)
+	}
+}
+
+func (a *app) routeLogin(e *echo.Echo) {
 
 	staticClaims := map[string]interface{}{
 		"xfoo":  "bar",
 		"xtime": time.Now().Unix(),
 	}
 
-	return func(c *echo.Context) error {
-		ts, _, err := jm.GenerateToken(staticClaims)
+	e.Get("/login", func(c *echo.Context) error {
+		ts, _, err := a.jwtSrv.GenerateToken(staticClaims)
 		if err != nil {
 			return err
 		}
-
 		return c.String(http.StatusOK, ts)
-	}
+	})
 }
 
-func setupStoreRoutes(g *echo.Group, dbrSess *dbr.Session) {
-	sm := store.NewManager(
-		store.NewStorageOption(),
-	)
-	if err := sm.ReInit(dbrSess); err != nil {
-		log.Fatal("sm.ReInit(dbc.NewSession())", "err", err)
-	}
+func (a *app) setupStoreRoutes(e *echo.Echo) {
 
-	g.Get(store.RouteStores, store.RESTStores(sm))
+	//	eg1 := e.Group(httputils.APIRoute.String(), a.jwtSrv.WithParseAndValidate())
+
+	//	eg1.Get(store.RouteStores, store.RESTStores(sm))
 	//	eg1.POST(store.RouteStores, store.RESTStoreCreate)
 	//	eg1.GET(store.RouteStore, store.RESTStore)
 	//	eg1.PUT(store.RouteStore, store.RESTStoreSave)
 	//	eg1.DELETE(store.RouteStore, store.RESTStoreDelete)
 
+}
+
+func main() {
+	a := newApp()
+	defer a.close() // @todo check signal and close gracefully
+
+	e := echo.New()
+
+	e.Use(mw.Logger())
+	//e.Use(mw.Recover())
+	//	e.SetDebug(true)
+
+	a.routeLogin(e)
+	a.setupStoreRoutes(e)
+
+	println("Starting server @ ", ServerAddress)
+	e.Run(ServerAddress)
 }
